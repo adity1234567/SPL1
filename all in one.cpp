ebc6e3f516478d6d67ae89122cba9773519aacde
Adity_Khisa <113276761+adity1234567@users.noreply.github.com>
2023-05-20 23:52:43 +0600

    Update all in one.cpp

--- all in one.cpp
+++ all in one.cpp
@@ -285,7 +285,7 @@ void power_and_constant(string power,string constant)
    }
    else
    {
-       cout<<"("<<numerator_two_int/numerator_constant_int<<")";
+       cout<<"("<<simplification_numerator_denominator(convert_int_to_string(numerator_constant_int),convert_int_to_string(numerator_two_int))<<")";
 
        cout<<main_function<<power1<<numerator_two_int<<endl;
    }
@@ -835,6 +835,10 @@ void calculate_trigonmetry_part(string input,string coefficient,string constant,
     {
         change_function+="(-1)*cot";
     }
+    else if(function_is=="tanx")
+    {
+         change_function+="ln(sec";
+    }
 
     cout<<"change function:\ "<<change_function<<endl;
 
@@ -1144,6 +1148,87 @@ int extract_constant(const string& input) {
 
     return leadingNumber * sign;
 }
+
+
+///uv
+int extract_power(const char* str)
+ {
+    int exponent = 0;
+    const char* ptr = str;
+
+    while (*ptr != '\0')
+    {
+        if (*ptr == '^')
+        {
+            ptr++;
+            while (*ptr >= '0' && *ptr <= '9')
+            {
+                exponent = exponent * 10 + (*ptr - '0');
+                ptr++;
+            }
+            break;
+        }
+        ptr++;
+    }
+
+    return exponent;
+}
+
+
+the_series_of_x(int main_power,int constant,int i)
+{
+   vector<string>all_x;
+   string with_x;
+   string new_power=convert_int_to_string(main_power);
+   string new_constant=convert_int_to_string(constant),new_i;
+   new_i=convert_int_to_string(i);
+
+   if(i%2!=0)
+   {
+       with_x="((x^"+new_power+")/"+new_constant+"^"+new_i+")";
+       all_x.push_back(with_x);
+       with_x="-";
+   }
+   else
+   {
+       with_x="((x^"+new_power+")/"+new_constant+"^"+new_i+")";
+       all_x.push_back(with_x);
+       with_x="+";
+   }
+   the_series_of_x(main_power-1,constant,i+1);
+}
+
+/*uv_e_mx(string equation)
+{
+   // char str[] = "x^2*e^(4*x)";
+   string equation1=equation,equation2=equation;
+   int power=0,constant=0;
+   //for removing
+    const char delimiter[] = "*e";
+    char* match1 = strstr(equation1, delimiter);
+    if (match1 != NULL)
+    {
+        *match1 = '\0';
+    }
+
+    //for using e...
+    char* match2 = strstr(equation2, delimiter);
+    if (match2 != NULL)
+    {
+        char* substring = match2 + strlen(delimiter);
+
+    }
+
+   power=extract_power(equation1);
+   constant=extract_power(substring);
+   int i=1;
+    the_series_of_x(new_power,power,i);
+
+
+
+}
+*/
+
 trigonmetry_diff_coefficient(string equation)
 {
     ///sin(5*x)cos(4*x)
